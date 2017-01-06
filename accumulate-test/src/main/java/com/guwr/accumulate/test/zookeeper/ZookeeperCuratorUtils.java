48a07fc1a83140d3ae202de53e26d62f38f2be2f
395994939@qq.com <395994939@qq.com>
2017-01-06 16:23:16 +0800

    guwr

--- accumulate-test/src/main/java/com/guwr/accumulate/test/zookeeper/ZookeeperCuratorUtils.java
+++ accumulate-test/src/main/java/com/guwr/accumulate/test/zookeeper/ZookeeperCuratorUtils.java
@@ -3,10 +3,8 @@ package com.guwr.accumulate.test.zookeeper;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.framework.api.GetChildrenBuilder;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-
-import java.util.List;
 
 /**
  * Created by   guwr
@@ -17,14 +15,32 @@ import java.util.List;
  * Description
  */
 public class ZookeeperCuratorUtils {
-    public static void main(String[] args) throws Exception{
-        String connectString = "192.168.2.249:12181";
+    public static void main(String[] args) throws Exception {
+        String connectString = "192.168.3.143:2181";
+        //操作失败重试机制 1000毫秒间隔 重试3次
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        //创建Curator客户端
         CuratorFramework client = CuratorFrameworkFactory.newClient(connectString, retryPolicy);
+        //开始
         client.start();
-        GetChildrenBuilder children = client.getChildren();
-        System.out.println("children = " + children);
-        List<String> strings = children.forPath("/");
-        System.out.println("strings = " + strings);
+        /**
+         * 这个类是线程安全的，一个JVM创建一个就好
+         * mylock 为锁的根目录，我们可以针对不同的业务创建不同的根目录
+         */
+        final InterProcessMutex lock = new InterProcessMutex(client, "/mylock");
+
+        try {
+            //阻塞方法，获取不到锁线程会挂起。
+            lock.acquire();
+            System.out.println("已经获取到锁");
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //释放锁，必须要放到finally里面，已确保上面方法出现异常时也能够释放锁。
+            lock.release();
+        }
+        Thread.sleep(10000);
+        client.close();
     }
 }
